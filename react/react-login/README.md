d90157072146fc385de9ffe615cd9710248554be
shunwuyu <shunwu2001@163.com>
2021-10-18 09:49:38 +0800

    history

--- react/react-login/README.md
+++ react/react-login/README.md
@@ -1,3 +1,4 @@
 [source](https://juejin.cn/post/6984667855115517988#heading-2)
+[source](https://juejin.cn/post/6844904021233238024)
 - redirect 
 - useHistory 跳home
--- react/react-login/src/index.js
+++ react/react-login/src/index.js
@@ -5,18 +5,25 @@ import App from './App';
 import Login from './Login';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import LifeCycle from './LifeCycle';
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/home" component={App} />
-        <Redirect path="/" to="/login" exact />
-      </Switch>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <>
+    <LifeCycle />
+  </>,
   document.getElementById('root')
-);
+)
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <Switch>
+//         <Route path="/login" component={Login} />
+//         <Route path="/home" component={App} />
+//         <Redirect path="/" to="/login" exact />
+//       </Switch>
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
